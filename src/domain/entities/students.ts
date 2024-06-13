b81cee9e7536fdca31e4dab704c25747d169b745
Guilhermecheng <guilherme.cheng@gmail.com>
2024-06-13 19:37:43 -0300

    abstracting entity creation

--- src/domain/entities/students.ts
+++ src/domain/entities/students.ts
@@ -1,9 +1,14 @@
 import { Entity } from "../../core/entities/entity"
+import { UniqueEntityID } from "../../core/entities/unique-entity-id";
 
-interface StudentsProps {
+interface StudentProps {
     name: string;
 }
 
-export class Students extends Entity<StudentsProps> {
+export class Students extends Entity<StudentProps> {
+    static create(props: StudentProps, id?: UniqueEntityID) {
+        const student = new Students(props , id);
 
+        return student
+    }
 }
