1ae40ac3ef68920f6e88029fc0b1c5eb5f221bba
Guilhermecheng <guilherme.cheng@gmail.com>
2024-06-20 00:25:09 -0300

    feat: edit question use case

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -13,6 +13,11 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
         this.items.splice(itemIndex, 1)
     }
 
+    async save(question: Question): Promise<void> {
+        const itemIndex = this.items.findIndex(item => item.id === question.id);
+        this.items[itemIndex] = question
+    }
+
     async findById(id: string) {
         const question = this.items.find(item => item.id.toString() === id)
 
