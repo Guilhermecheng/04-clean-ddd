dbcce58edac66d701f1df7d4919e481373749d23
Guilhermecheng <guilherme.cheng@gmail.com>
2024-08-22 12:39:14 -0300

    feat: best question chosen subscriber

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -1,3 +1,4 @@
+import { DomainEvents } from "@/core/events/domain-events";
 import { PaginationParams } from "@/core/repositories/pagination-params";
 import { QuestionAttachmentsRepository } from "@/domain/forum/application/repositories/question-attachments-repository";
 import { QuestionsRepository } from "@/domain/forum/application/repositories/questions-repository";
@@ -10,6 +11,8 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
 
     async create(question: Question) {
         this.items.push(question)
+
+        DomainEvents.dispatchEventsForAggregate(question.id)
     }
 
     async delete(question: Question) {
@@ -22,6 +25,8 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
     async save(question: Question): Promise<void> {
         const itemIndex = this.items.findIndex(item => item.id === question.id);
         this.items[itemIndex] = question
+
+        DomainEvents.dispatchEventsForAggregate(question.id)
     }
 
     async findById(id: string) {
